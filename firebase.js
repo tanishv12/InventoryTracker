7d89f862c11766545aa2a9cb07914c7129708df5
Tanish Visanagiri <tanishv12@gmail.com>
2024-08-18 19:55:14 +0530

    firebase auth works

--- firebase.js
+++ firebase.js
@@ -9,7 +9,7 @@ import { getVertexAI, getGenerativeModel } from "firebase/vertexai-preview";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCem1fY9EiSTKNQXx4P3M_MNdeXFYV7p0k',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: 'inventorytracker-8ad38.firebaseapp.com',
   projectId: 'inventorytracker-8ad38',
   storageBucket: 'inventorytracker-8ad38.appspot.com',
